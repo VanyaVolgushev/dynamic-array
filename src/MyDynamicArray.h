a59d834b15e5dbe8d7612203864d49dc4940f890
Your Name <ivo.volgushev@yandex.ru>
2025-12-27 12:10:59 +0300

    Add more stuff

--- src/MyDynamicArray.h
+++ src/MyDynamicArray.h
@@ -3,6 +3,8 @@
 #include <type_traits>
 #include <cassert>
 #include <iterator>
+#include <cstdlib>
+#include <utility>
 
 template<typename T>
 class MyDynamicArray final {
@@ -10,6 +12,9 @@ public:
     MyDynamicArray();
     MyDynamicArray(size_t capacity);
     ~MyDynamicArray();
+
+    MyDynamicArray(MyDynamicArray const& other);
+    MyDynamicArray(MyDynamicArray&& other) noexcept;
 
     size_t insert(T const& value);
     size_t insert(size_t index, T const& value);
@@ -21,7 +26,7 @@ public:
     size_t size() const;
     size_t capacity() const;
     
-class Iterator {
+    class Iterator {
     public:
         using iterator_category = std::bidirectional_iterator_tag;
         using value_type        = T;
@@ -45,7 +50,6 @@ class Iterator {
         Iterator& operator--() { --offset; return *this; }
         Iterator  operator--(int) { Iterator tmp(*this); --(*this); return tmp; }
     
-        // return a reference so iterator behaves like a normal container iterator
         reference operator*() const { return *(p + offset); }
         pointer   operator->() const { return p + offset; }
     
@@ -54,7 +58,6 @@ class Iterator {
         size_t offset;
     };
     
-    // add typedef for reverse iterator and the rbegin/rend functions
     using reverse_iterator = std::reverse_iterator<Iterator>;
     
     Iterator begin() { return Iterator(0, _data); }
@@ -78,7 +81,7 @@ private:
 // ------------------------------------------------------------------------
 
 template<typename T>
-MyDynamicArray<T>::MyDynamicArray() : MyDynamicArray(DEFAULT_CAPACITY) {};
+MyDynamicArray<T>::MyDynamicArray() : MyDynamicArray(DEFAULT_CAPACITY) {}
 
 template<typename T>
 MyDynamicArray<T>::MyDynamicArray(size_t capactiy) {
@@ -89,11 +92,38 @@ MyDynamicArray<T>::MyDynamicArray(size_t capactiy) {
 
 template<typename T>
 MyDynamicArray<T>::~MyDynamicArray() {
-    for (size_t i = 0; i < _size; i++)
-    {
-        _data[i].~T();
+    if (_data) {
+        for (size_t i = 0; i < _size; i++)
+        {
+            _data[i].~T();
+        }
+        free(_data);
     }
-    free(_data);
+}
+
+template<typename T>
+MyDynamicArray<T>::MyDynamicArray(MyDynamicArray const& other) {
+    _capacity = other._capacity;
+    _size = other._size;
+    if (_capacity == 0) {
+        _data = nullptr;
+        return;
+    }
+    _data = static_cast<T*>(malloc(sizeof(T) * _capacity));
+    for (size_t i = 0; i < _size; ++i) {
+        new (&_data[i]) T(other._data[i]);
+    }
+}
+
+template<typename T>
+MyDynamicArray<T>::MyDynamicArray(MyDynamicArray&& other) noexcept {
+    _capacity = other._capacity;
+    _size = other._size;
+    _data = other._data;
+
+    other._capacity = 0;
+    other._size = 0;
+    other._data = nullptr;
 }
 
 template <typename T>
@@ -161,12 +191,12 @@ size_t MyDynamicArray<T>::capacity() const
 
 template <typename T>
 void MyDynamicArray<T>::upsize() {
-    size_t new_capacity = _capacity * RESIZE_FACTOR;
+    size_t new_capacity = (_capacity == 0) ? DEFAULT_CAPACITY : static_cast<size_t>(_capacity * RESIZE_FACTOR);
     T* new_data = (T*)malloc(sizeof(T) * new_capacity);
     for (size_t i = 0; i < _size; i++) {
         new (&new_data[i]) T(std::move(_data[i]));
     }
-    free(_data);
+    if (_data) free(_data);
     _data = new_data;
     _capacity = new_capacity;
 };
--- tests/test_myDynamicArray_copy_move.cpp
+++ tests/test_myDynamicArray_copy_move.cpp
@@ -1,20 +1,47 @@
-#include <gtest/gtest.h>
 #include "MyDynamicArray.h"
+#include <gtest/gtest.h>
+#include <string>
 
-TEST(MyDynamicArrayTest, CopyConstructor) {
-    MyDynamicArray<int> arr;
-    EXPECT_EQ(arr.size(), 0);
-    EXPECT_EQ(arr.capacity(), 16);
+TEST(MyDynamicArrayCopyTest, CopyConstructorWithStrings) {
+    MyDynamicArray<std::string> a;
+    a.insert(std::string("one"));
+    a.insert(std::string("two"));
+    a.insert(std::string("three"));
+
+    // copy-construct
+    MyDynamicArray<std::string> b(a);
+
+    // sizes should match
+    ASSERT_EQ(a.size(), b.size());
+    ASSERT_EQ(a.capacity(), b.capacity());
+
+    // elements should be equal
+    for (size_t i = 0; i < a.size(); ++i) {
+        EXPECT_EQ(a[i], b[i]);
+    }
+
+    // modifying original should not change the copy (deep copy)
+    a[0] = "changed";
+    EXPECT_EQ(b[0], "one");
+    EXPECT_EQ(a[0], "changed");
 }
 
-TEST(MyDynamicArrayTest, CopyConstructor) {
-    MyDynamicArray<int> arr;
-    EXPECT_EQ(arr.size(), 0);
-    EXPECT_EQ(arr.capacity(), 16);
-}
+TEST(MyDynamicArrayMoveTest, MoveConstructorWithStrings) {
+    MyDynamicArray<std::string> a;
+    a.insert(std::string("alpha"));
+    a.insert(std::string("beta"));
+    a.insert(std::string("gamma"));
 
-TEST(MyDynamicArrayTest, CopyConstructor) {
-    MyDynamicArray<int> arr;
-    EXPECT_EQ(arr.size(), 0);
-    EXPECT_EQ(arr.capacity(), 16);
+    // move-construct
+    MyDynamicArray<std::string> moved(std::move(a));
+
+    // moved should have elements
+    ASSERT_EQ(moved.size(), 3u);
+    EXPECT_EQ(moved[0], "alpha");
+    EXPECT_EQ(moved[1], "beta");
+    EXPECT_EQ(moved[2], "gamma");
+
+    // original should be in a valid empty state
+    EXPECT_EQ(a.size(), 0u);
+    EXPECT_EQ(a.capacity(), 0u);
 }
